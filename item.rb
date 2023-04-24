35469d62aa0898c933b2fa0a3f69fe7822b49e97
Eric <eric2umeh@yahoo.com>
2023-04-24 16:08:20 +0100

    Create item.rb

---
+++ item.rb
@@ -0,0 +1,32 @@
+require 'date'
+
+class Item
+  attr_reader :id, :archived
+  attr_accessor :genre, :author, :source, :label, :publish_date
+
+  def initialize(genre, author, source, label, publish_date)
+    @id = rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+      puts 'Item has been archieved'
+    else
+      puts 'Item has not been moved to archieved'
+    end
+  end
+
+  def can_be_archived?
+    publish_year = @publish_date.year
+    current_year = Date.current.year
+    age_in_years = current_year - publish_year
+    age_in_years > 10
+  end
+end
