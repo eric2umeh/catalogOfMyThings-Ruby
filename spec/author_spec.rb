d774176da0ba31a17059bd7406d1a868b450de5d
Ermi <manermidem@gmail.com>
2023-04-25 21:26:42 +0300

    Add a test for the add_item method in author.file

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../author.rb'
+require_relative '../item.rb'
+require_relative '../book.rb'
 
 describe Author do
 
@@ -18,4 +20,28 @@ describe Author do
         expect(@author.last_name).to eq("Demsew")
     end
     
+end
+
+describe Item do
+
+    before :each do
+        @author = Author.new("Ermiyas", "Demsew")
+        @book = Book.new('DC Comics', 'bad', '2022-04-03')
+        @item = Item.new('2022-04-03')
+
+        end
+
+    it "should be an instance of Item class" do
+        expect(Item.new("2012-04-03")).to be_an_instance_of(Item)
+    end
+
+    it "Should return the correct value for the publish_date attribute" do
+        expect(Item.new("2012-04-03").publish_date).to eq("2012-04-03")
+    end
+
+    it 'Should return add_item' do
+        @author.add_item(@book)
+        expect(@author.items).to eq([@book])
+    end
+
 end
