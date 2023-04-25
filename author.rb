5266e5448099c5d08577f48cebad7204afad5d61
Ermi <manermidem@gmail.com>
2023-04-25 13:37:13 +0300

    Fix linters error

--- author.rb
+++ author.rb
@@ -1,18 +1,18 @@
 require_relative 'item'
 
-class Author 
-    attr_reader :id
-    attr_accessor :first_name, :last_name, :items
+class Author
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :items
 
-    def initialize(first_name, last_name)
-        @id = Random.rand(1..1000)
-        @first_name = first_name
-        @last_name = last_name
-         @items = []
-    end 
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item unless @items.include?(item)
-        item.author = self unless item.author == self
-    end
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.author = self unless item.author == self
+  end
 end
--- game.rb
+++ game.rb
@@ -1,19 +1,18 @@
 require_relative 'item'
 
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at
 
-    attr_accessor :multiplayer, :last_played_at
-    def initialize(multiplayer, last_played_at:date)
-        super(genre, author, source, label, publish_date)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(multiplayer, last_played_at: date)
+    super(genre, author, source, label, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def can_be_archived? 
-        if Item.can_be_archived? && @last_played_at > 2
-            return true
-        else
-            return false
-        end
-    end
+  def can_be_archived?
+    return true if Item.can_be_archived? && @last_played_at > 2
+
+
+    false
+  end
 end
