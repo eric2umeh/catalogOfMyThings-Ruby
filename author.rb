bbc903dee5337369c3f6c67398c4ae8c604d3274
Ermi <manermidem@gmail.com>
2023-04-25 13:35:16 +0300

    Implement a method called add_item for the author

--- author.rb
+++ author.rb
@@ -10,4 +10,9 @@ class Author
         @last_name = last_name
          @items = []
     end 
+
+    def add_item(item)
+        @items << item unless @items.include?(item)
+        item.author = self unless item.author == self
+    end
 end
