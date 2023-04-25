5337bc068401ff9117cb803dd12209c99d4ea263
Eric <eric2umeh@yahoo.com>
2023-04-25 11:46:54 +0100

    Update item.rb

--- item.rb
+++ item.rb
@@ -2,33 +2,24 @@ require 'date'
 
 class Item
   attr_reader :id, :archived
-  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_accessor :genre, :author, :label, :publish_date
 
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(publish_date)
     @id = rand(1..1000)
     @genre = genre
     @author = author
-    @source = source
     @label = label
     @publish_date = publish_date
     @archived = false
   end
 
   def move_to_archive
-    if can_be_archived?
-      @archived = true
-      puts 'Item has been archieved'
-    else
-      puts 'Item has not been moved to archieved'
-    end
+    @archived = can_be_archived?
   end
-
-  private
 
   def can_be_archived?
-    publish_year = @publish_date.year
-    current_year = Date.current.year
-    age_in_years = current_year - publish_year
-    age_in_years > 10
+    Date.today > Date.iso8601(@publish_date).next_year(10)
   end
+
+  private :can_be_archived?
 end
