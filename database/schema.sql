fd4110fac08c127ac40ce7ed4145f63c644b2002
Eric <eric2umeh@yahoo.com>
2023-04-26 17:36:37 +0100

    Update schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -17,3 +17,22 @@ CREATE TABLE games (
     FOREIGN KEY (author_id) REFERENCES author (id),
     FOREIGN KEY (genre_id) REFERENCES genre (id)
 );
+
+CREATE TABLE labels (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  color VARCHAR(120)
+);
+
+CREATE TABLE book (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publisher VARCHAR(100) NOT NULL,
+  cover_state VARCHAR(50) NOT NULL,
+  published_date DATE NOT NULL,
+  archived BOOLEAN,
+  label_id INT,
+  author_id INT,
+  genre_id INT,
+  CONSTRAINT book_fk
+   FOREIGN KEY(label_id) REFERENCES labels(id) ON DELETE CASCADE
+);
