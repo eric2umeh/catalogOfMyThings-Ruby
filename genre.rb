c9aef916e4581c5bd3c4d8bc178ac0ac10e446bf
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-04-26 23:58:45 +0500

    Update Genre.rb

--- genre.rb
+++ genre.rb
@@ -1,9 +1,10 @@
-require 'json'
+require_relative 'musicalbum'
 
 class Genre
   attr_accessor :name, :items
 
   def initialize(name)
+    @id = rand(1..1000)
     @name = name
     @items = []
   end
@@ -11,19 +12,5 @@ class Genre
   def add_item(item)
     @items << item
     item.genre = self
-  end
-
-  def to_json(*args)
-    {
-      'name' => @name,
-      'items' => @items.map(&:to_json)
-    }.to_json(*args)
-  end
-
-  def self.from_json(json)
-    data = JSON.parse(json)
-    genre = new(data['name'])
-    genre.items = data['items'].map { |item_data| MusicAlbum.from_json(item_data) }
-    genre
   end
 end
