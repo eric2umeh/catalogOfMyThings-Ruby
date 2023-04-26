52cc6e7cb543453cd7ffd2132f1fc7074d0ddf2b
Ermi <manermidem@gmail.com>
2023-04-26 15:07:58 +0300

    Add a method to save game data into the json file.

--- game_data.rb
+++ game_data.rb
@@ -1,4 +1,13 @@
 require 'json'
 
 module GameData
+    def save_game_data
+        @game_data = []
+        @games.each do |game|
+          @game_data.push('multiplayer' => game.multiplayer,
+                          'last_played_at' => game.last_played_at,
+                          'publish_date' => game.publish_date)
+        end
+        File.write('preserve_data/game_data.json', JSON.pretty_generate(@game_data))
+      end
   end
