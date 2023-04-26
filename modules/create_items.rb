da98ef1f1ae44dac77e074801fe586191277b4af
Ermi <manermidem@gmail.com>
2023-04-26 12:01:02 +0300

    Add a method to create game with its data

--- modules/create_items.rb
+++ modules/create_items.rb
@@ -24,7 +24,7 @@ module NewItem
   end
 
   def create_item
-    until %w[1].include?(@item_options)
+    until %w[1, 2].include?(@item_options)
       item_options
       @item_options = gets.chomp
       add_selected_item
@@ -53,5 +53,21 @@ module NewItem
     @books.push(book)
     puts 'Your book has been created successfully!'
     puts '********************************'
+  end
+
+  def create_game
+    print 'Enter the last played at (yyyy-mm-dd):'
+    last_played_at = gets.chomp
+    print 'Is It multiplayer(y/n):'
+    multiplayer = gets.chomp.downcase
+    multiplayer = multiplayer == 'y'
+    print 'Enter the publish date (yyyy-mm-dd):'
+    publish_date = gets.chomp
+    game = Game.new(last_played_at, multiplayer, publish_date)
+    author = author_game
+    author.add_item(game)
+    @games << game
+    puts 'Game has been created successfully!!!'
+    puts '***************************************'
   end
 end
